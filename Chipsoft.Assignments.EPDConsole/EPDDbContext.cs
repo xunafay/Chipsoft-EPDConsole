e11b81a5fdd7511f69a4aa79406275324e5cf4a6
CSHDJO <99656699+CSHDJO@users.noreply.github.com>
2023-03-17 10:57:30 +0100

    Create assignment

---
+++ Chipsoft.Assignments.EPDConsole/EPDDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chipsoft.Assignments.EPDConsole
+{
+    public class EPDDbContext : DbContext
+    {
+        // The following configures EF to create a Sqlite database file in the
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source=epd.db");
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
---
+++ Chipsoft.Assignments.EPDConsole/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Chipsoft.Assignments.EPDConsole
+{
+    public class Program
+    {
+        //Don't create EF migrations, use the reset db option
+        //This deletes and recreates the db, this makes sure all tables exist
+
+        private static void AddPatient()
+        {
+            //Do action
+            //return to show menu again.
+        }
+
+        private static void DeleteAppointment()
+        {
+        }
+
+        private static void AddAppointment()
+        {
+        }
+
+        private static void DeletePhysician()
+        {
+        }
+
+        private static void AddPhysician()
+        {
+        }
+
+        private static void DeletePatient()
+        {
+        }
+
+
+        #region FreeCodeForAssignment
+        static void Main(string[] args)
+        {
+            while (ShowMenu())
+            {
+                //Continue
+            }
+        }
+
+        public static bool ShowMenu()
+        {
+            Console.Clear();
+            foreach (var line in File.ReadAllLines("logo.txt"))
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("1 - Patient toevoegen");
+            Console.WriteLine("2 - Patienten verwijderen");
+            Console.WriteLine("3 - Arts toevoegen");
+            Console.WriteLine("4 - Arts verwijderen");
+            Console.WriteLine("5 - Afspraak toevoegen");
+            Console.WriteLine("6 - Afspraken inzien");
+            Console.WriteLine("7 - Sluiten");
+            Console.WriteLine("8 - Reset db");
+
+            if (int.TryParse(Console.ReadLine(), out int option))
+            {
+                switch (option)
+                {
+                    case 1:
+                        AddPatient();
+                        return true;
+                    case 2:
+                        DeletePatient();
+                        return true;
+                    case 3:
+                        AddPhysician();
+                        return true;
+                    case 4:
+                        DeletePhysician();
+                        return true;
+                    case 5:
+                        AddAppointment();
+                        return true;
+                    case 6:
+                        DeleteAppointment();
+                        return true;
+                    case 7:
+                        return false;
+                    case 8:
+                        EPDDbContext dbContext = new EPDDbContext();
+                        dbContext.Database.EnsureDeleted();
+                        dbContext.Database.EnsureCreated();
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+            return true;
+        }
+
+        #endregion
+    }
+}
